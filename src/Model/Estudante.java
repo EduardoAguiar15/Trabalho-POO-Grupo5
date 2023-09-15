993b6894591fdb8f97748a21ff296ed1b9216f48
EduardoAguiar15 <aguiareduardo678@outlook.com>
2023-09-15 18:44:45 -0300

    Projeto final da disciplina de Programação orientada a objeto

--- src/Model/Estudante.java
+++ src/Model/Estudante.java
@@ -1,10 +1,6 @@
 package Model;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Estudante {
-
 	private int id;
 	private String nome;
 	private String curso;
@@ -40,37 +36,7 @@ public class Estudante {
 		this.curso = curso;
 	}
 
-	public static ArrayList<Estudante> organizarEstudantes(Integer qtEstudantes) {
-		ArrayList<Estudante> estudantes = new ArrayList<>();
-
-		String[] nomes = { "Ana", "Luís", "Maria", "João", "Mariana", "Carlos", "Isabel", "Rafael", "Sofia", "Pedro",
-				"Clara", "António", "Lara", "Tiago", "Beatriz", "Gustavo", "Lúcia", "Rui", "Laura", "Miguel", "Teresa",
-				"André", "Catarina", "Fernando", "Inês", "Manuel", "Rita", "Paulo", "Camila", "Joaquim", "Carolina",
-				"Vasco", "Marta", "Ricardo", "Diana", "Daniel", "Bianca", "Hugo", "Raquel", "Francisco", "Vitória",
-				"Leonardo", "Eva", "Gilberto", "Cláudia", "Simão", "Gabriela", "Hélder", "Amanda", "Guilherme" };
-		String[] cursos = { "Administração de Empresas", "Engenharia Civil", "Psicologia", "Medicina",
-				"Arquitetura e Urbanismo", "Direito", "Ciências da Computação", "Enfermagem", "Marketing",
-				"Contabilidade", "Design Gráfico", "Economia", "Educação Física", "Nutrição", "Jornalismo", "Biologia",
-				"Matemática", "Física", "Química", "História", "Geografia", "Letras", "Ciências Sociais", "Filosofia",
-				"Sociologia", "Pedagogia", "Engenharia Elétrica", "Engenharia Mecânica", "Engenharia de Produção",
-				"Administração Pública", "Gestão de Recursos Humanos", "Farmácia", "Odontologia",
-				"Ciências da Comunicação", "Ciências Ambientais", "Artes Cênicas", "Música", "Dança",
-				"Gestão de Projetos", "Fotografia", "Ciência Política", "Relações Internacionais", "Teatro",
-				"Design de Moda", "Engenharia Química", "Medicina Veterinária", "Gestão de Marketing Digital",
-				"Gastronomia", "Hotelaria e Turismo", "Engenharia de Software" };
-
-		Random r = new Random();
-
-		for (int i = 0; i < qtEstudantes; i++) {
-			estudantes.add(new Estudante(i + 1, nomes[r.nextInt(nomes.length)], cursos[r.nextInt(cursos.length)]));
-		}
-
-		return estudantes;
+	public Estudante() {
+		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public String toString() {
-		return "Estudante [id=" + id + ", nome=" + nome + ", curso=" + curso + "]";
-	}
-
 }
--- src/View/Menu.java
+++ src/View/Menu.java
@@ -1,8 +1,10 @@
 package View;
 
-import java.sql.Connection;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import Data.BancoDeDados;
 import Model.Estudante;
@@ -10,24 +12,19 @@ import Model.Estudante;
 public class Menu {
 
 	private BancoDeDados bancoDeDados;
-	private static Scanner scanner;
+	private Scanner scanner;
 
-	public Menu() {
-
-		ArrayList<Estudante> estudantes = Estudante.organizarEstudantes(50);
-		Connection conexao = (Connection) BancoDeDados.obterConexao();
-
-		scanner = new Scanner(System.in);
+	public Menu(BancoDeDados bancoDeDados, Scanner scanner) {
+		super();
+		this.bancoDeDados = bancoDeDados;
+		this.scanner = scanner;
 	}
 
-	@Override
-	public String toString() {
-		return "Menu [bancoDeDados=" + bancoDeDados + ", scanner=" + scanner + "]";
-	}
-
-	public void exibirMenu() {
+	public void exibirMenu() throws InterruptedException {
 		boolean continuar = true;
 
+		System.out.println("Bem-vindo ao Sistema de Gerenciamento de Estudantes!\n");
+		Thread.sleep(3000);
 		while (continuar) {
 			System.out.println("\nMenu de Opções:");
 			System.out.println("1. Adicionar Estudante");
@@ -36,84 +33,265 @@ public class Menu {
 			System.out.println("4. Listar Estudantes");
 			System.out.println("5. Sair");
 			System.out.print("\nEscolha uma opção: ");
+			String entrada = scanner.nextLine();
+			System.out.print("\n");
 
-			int escolha = scanner.nextInt();
-			scanner.nextLine();
+			int resposta = 0;
 
 			try {
+				int escolha = Integer.parseInt(entrada);
 				switch (escolha) {
 				case 1:
-					adicionarEstudante();
+					chamar(resposta, () -> {
+						try {
+							adicionarEstudante();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					});
 					break;
 				case 2:
-					editarEstudante();
+					chamar(resposta, () -> {
+						try {
+							editarEstudante();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					});
 					break;
 				case 3:
-					removerEstudante();
+					chamar(resposta, () -> {
+						try {
+							removerEstudante();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					});
 					break;
 				case 4:
-					listarEstudantes();
+					chamar(resposta, () -> {
+						try {
+							listarEstudantes();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					});
 					break;
 				case 5:
 					continuar = false;
+					sair();
 					break;
 				default:
 					System.out.println("Opção inválida.");
+					Thread.sleep(2000);
 				}
 
 			} catch (Exception e) {
-				System.out.println("Erro: " + e.getMessage());
-
+				System.out.println("\nErro: " + e.getMessage());
+				Thread.sleep(3000);
 			}
 
 		}
 		scanner.close();
 	}
 
-	private void adicionarEstudante() {
-		try {
-			System.out.println("Nome: ");
-			String nome = scanner.nextLine();
+	private void chamar(int resposta, RealizarAcao acao) {
+		acao.executarAcao();
+		while (resposta == 1 || resposta == 2 || resposta == 0) {
+			System.out.println("Continuar(1)  Voltar ao menu principal(2)");
+			String resp = scanner.nextLine();
+			try {
+				int escolha = Integer.parseInt(resp);
+				switch (escolha) {
+				case 1:
+					acao.executarAcao();
+					break;
 
-			System.out.println("\nCurso: ");
-			String curso = scanner.nextLine();
+				case 2:
+					resposta = 3;
+					break;
 
-			System.out.println("Estudante adicionado com sucesso!");
-		} catch (Exception e) {
-			System.out.println("Erro ao adicionar estudante: " + e.getMessage());
-		}
-	}
-
-	private void editarEstudante() {
-		try {
-			// if/else?
-		} catch (Exception e) {
-			System.out.println("Erro ao editar estudante: " + e.getMessage());
-
-		}
-	}
-
-	private void removerEstudante() {
-		try {
-			// lógica
-			System.out.println("Estudante removido com sucesso.");
-		} catch (Exception e) {
-			System.out.println("Erro ao remover estudante: " + e.getMessage());
-		}
-	}
-
-	private void listarEstudantes() {
-		try {
-			// lógica
-			ArrayList<Estudante> estudantes = Estudante.organizarEstudantes(50);
-			System.out.println("Lista de estudantes: ");
-			for (Estudante e : estudantes) {
-				System.out.println(e.getNome() + e.getCurso());
+				default:
+					System.out.println("Opção inválida.");
+				}
+			} catch (Exception e) {
+				System.out.println("Erro: " + e.getMessage());
 
 			}
-		} catch (Exception e) {
-			System.out.println("Erro ao listar estudantes: " + e.getMessage());
+
 		}
 	}
 
+	private void adicionarEstudante() throws InterruptedException {
+		try {
+			Estudante estudante = new Estudante();
+
+			System.out.println("Nome: ");
+			estudante.setNome(scanner.nextLine());
+			if (verificaSemNumeros(estudante.getNome()) || estudante.getNome().length() < 2) {
+				System.out.println("Erro. Nome deve conter mais de um caractere e não deve conter número.");
+				Thread.sleep(3000);
+				return;
+			}
+			System.out.println("\nCurso: ");
+			estudante.setCurso(scanner.nextLine());
+			if (estudante.getCurso().length() == 0) {
+				System.out.println("curso inválido!");
+				Thread.sleep(2000);
+				return;
+			}
+			bancoDeDados.adicionarEstudante(estudante);
+		} catch (Exception e) {
+			System.out.println("Erro ao adicionar estudante: " + e.getMessage());
+			Thread.sleep(3000);
+		}
+	}
+
+	private void editarEstudante() throws InterruptedException, SQLException {
+		Estudante estudante = new Estudante();
+		List<Estudante> estudantes = bancoDeDados.listarEstudantes();
+		for (int i = 0; i < estudantes.size(); i++) {
+			Estudante e = estudantes.get(i);
+			if (i % 6 == 0 && i != 0) {
+				System.out.println();
+			}
+			System.out.printf("(" + e.getId() + ")" + e.getNome() + " | ");
+		}
+		System.out.println("\n\nDigite o ID do estudante que deseja editar: ");
+		String entrada = scanner.nextLine();
+
+		try {
+			int id = Integer.parseInt(entrada);
+			if (bancoDeDados.verificarExistencia(id)) {
+				System.out.println("O que você gostaria de editar?");
+				System.out.println("1. Nome");
+				System.out.println("2. Curso");
+				System.out.println("3. Ambos");
+				int opcao = scanner.nextInt();
+				scanner.nextLine();
+				if (opcao == 1) {
+					System.out.println("Novo nome: ");
+					String novoNome = scanner.nextLine();
+					if (verificaSemNumeros(novoNome) || novoNome.length() < 2) {
+						System.out.println("Erro. Nome deve conter mais de um caracter e não deve conter número.");
+						Thread.sleep(2000);
+						return;
+					}
+					estudante.setId(id); // Definindo o ID para o objeto estudante
+					estudante.setNome(novoNome); // Definindo o nome para o objeto estudante
+					bancoDeDados.editarEstudante(estudante.getId(), estudante.getNome(), null);
+				} else if (opcao == 2) {
+					System.out.println("Novo curso: ");
+					String novoCurso = scanner.nextLine();
+					if (novoCurso.length() == 0) {
+						System.out.println("curso inválido!");
+						Thread.sleep(2000);
+						return;
+					}
+					estudante.setId(id);
+					estudante.setCurso(novoCurso);
+					bancoDeDados.editarEstudante(estudante.getId(), null, estudante.getCurso());
+				} else if (opcao == 3) {
+					System.out.println("Novo nome: ");
+					String novoNome = scanner.nextLine();
+					if (verificaSemNumeros(novoNome) || novoNome.length() < 2) {
+						System.out.println("Erro. Nome deve conter mais de um caracter e não deve conter número.");
+						Thread.sleep(2000);
+						return;
+					}
+					System.out.println("Novo curso: ");
+					String novoCurso = scanner.nextLine();
+					if (novoCurso.length() == 0) {
+						System.out.println("curso inválido!");
+						Thread.sleep(2000);
+						return;
+					}
+					estudante.setId(id);
+					estudante.setNome(novoNome);
+					estudante.setCurso(novoCurso);
+					bancoDeDados.editarEstudante(estudante.getId(), estudante.getNome(), estudante.getCurso());
+				} else {
+					System.out.println("Opção inválida!");
+					Thread.sleep(2000);
+				}
+
+			} else {
+				System.out.println("Estudante não encontrado.");
+				Thread.sleep(2000);
+			}
+		} catch (Exception e) {
+			System.out.println("Erro ao editar estudante: " + e.getMessage());
+			Thread.sleep(2000);
+		}
+	}
+
+	private void removerEstudante() throws InterruptedException, SQLException {
+		List<Estudante> estudantes = bancoDeDados.listarEstudantes();
+		for (int i = 0; i < estudantes.size(); i++) {
+			Estudante estudante = estudantes.get(i);
+			if (i % 6 == 0 && i != 0) {
+				System.out.println();
+			}
+			System.out.printf("(" + estudante.getId() + ")" + estudante.getNome() + " | ");
+		}
+		System.out.println("\n\nDigite o ID do estudante que deseja remover: ");
+		String entrada = scanner.nextLine();
+		try {
+			int id = Integer.parseInt(entrada);
+			bancoDeDados.removerEstudante(id);
+
+		} catch (Exception e) {
+			System.out.println("Erro ao remover estudante: " + e.getMessage());
+			Thread.sleep(2000);
+		}
+	}
+
+	private void listarEstudantes() throws InterruptedException {
+		try {
+			List<Estudante> estudantes = bancoDeDados.listarEstudantes();
+
+			if (estudantes.isEmpty()) {
+				System.out.println("Nenhum estudante encontrado.");
+			} else {
+				System.out.println("\nLista de estudantes:");
+				System.out.println("\n/////////////////////////////////////////\n");
+				for (Estudante estudante : estudantes) {
+					System.out.println("ID: " + estudante.getId());
+					System.out.println("Nome: " + estudante.getNome());
+					System.out.println("Curso: " + estudante.getCurso());
+					System.out.println();
+					Thread.sleep(250);
+
+				}
+			}
+			System.out.println("\n/////////////////////////////////////////\n");
+		} catch (Exception e) {
+			System.out.println("Erro ao listar estudantes: " + e.getMessage());
+			Thread.sleep(2000);
+		}
+	}
+
+	public static void sair() {
+		System.out.println("Obrigado por usar o Sistema de Gerenciamento de Estudante!");
+		System.out.println("Programa Finalizado!");
+		System.exit(0);
+	}
+
+	public static boolean verificaSemNumeros(String texto) {
+		// Use uma expressão regular para verificar se não há dígitos na string
+		Pattern pattern = Pattern.compile(".*\\d.*");
+		Matcher matcher = pattern.matcher(texto);
+		return matcher.matches();
+	}
+
+}
+
+interface RealizarAcao {
+	void executarAcao();
 }
